ce3d0a7dea304b96d76c7f41fa8286fee8e98fd7
Gustavo Viscovini Vilas Boas <vbmarketingdgtal@gmail.com>
2026-02-24 23:21:58 -0300

    Aparencia APP

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
+    
       manifest: {
         name: 'VB Control',
         short_name: 'VB Control',
@@ -20,17 +18,18 @@ export default defineConfig({
         background_color: '#2b2b2b',
         theme_color: '#f9a705',
         icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+  {
+    src: '/icon-192.png',
+    sizes: '192x192',
+    type: 'image/png'
+  },
+  {
+    src: '/icon-512.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'any maskable'
+  }
+]
       }
     })
   ],
